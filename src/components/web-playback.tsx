06171bb2980711372fe9bb76e226ba3c15b2a4b4
biggestUaena24 <a2782541671@gmail.com>
2024-05-02 23:40:19 -0400

    This is not a working version of WebSdk, needs to implement backend to make web sdk work

---
+++ src/components/web-playback.tsx
@@ -0,0 +1,70 @@
+import { useState, useEffect } from "react";
+
+const track: any = {
+  name: "",
+  album: {
+    images: [{ url: "" }],
+  },
+  artists: [{ name: "" }],
+};
+
+export default function WebPlayback(props: any) {
+  const [is_paused, setPaused] = useState(false);
+  const [is_active, setActive] = useState(false);
+  const [player, setPlayer] = useState(undefined);
+  const [userTrack, setUserTrack] = useState(track);
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://sdk.scdn.co/spotify-player.js";
+    script.async = true;
+
+    document.body.appendChild(script);
+
+    window.onSpotifyWebPlaybackSDKReady = () => {
+      const player = new window.Spotify.Player({
+        name: "Web Playback SDK",
+        getOAuthToken: (cb: (arg0: any) => void) => {
+          cb(props.token);
+        },
+        volume: 0.5,
+      });
+
+      setPlayer(player);
+
+      player.addListener("ready", ({ device_id }: { device_id: string }) => {
+        console.log("Ready with Device ID", device_id);
+      });
+
+      player.addListener(
+        "not_ready",
+        ({ device_id }: { device_id: string }) => {
+          console.log("Device ID has gone offline", device_id);
+        }
+      );
+
+      player.addListener("player_state_changed", (state: any) => {
+        if (!state) {
+          return;
+        }
+
+        setUserTrack(state.track_window.current_track);
+        setPaused(state.paused);
+
+        player.getCurrentState().then((state: any) => {
+          !state ? setActive(false) : setActive(true);
+        });
+      });
+
+      player.connect();
+    };
+  }, []);
+
+  return (
+    <>
+      <div className="container">
+        <div className="main-wrapper"></div>
+      </div>
+    </>
+  );
+}
