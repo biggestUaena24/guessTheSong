ab07b85adbf2b0d862485bee6a15057725e900ff
biggestUaena24 <a2782541671@gmail.com>
2024-05-14 23:45:58 -0400

    Working version in local environment

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,18 +1,10 @@
-import { redirect } from "react-router-dom";
-
 export default function Login() {
-  async function onClick() {
-    const response = await fetch("http://localhost:1314/auth/login");
-    const { token } = await response.json();
-    localStorage.setItem("token", token);
-    if (localStorage.getItem("token")) {
-      redirect("/");
-    }
-  }
   return (
     <div className="App">
       <header className="App-header">
-        <h3 onClick={onClick}>Click to login</h3>
+        <a className="btn-spotify" href="http://localhost:1314/auth/login">
+          Login with Spotify
+        </a>
       </header>
     </div>
   );
--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,13 +1,21 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import Login from "./login";
 import WebPlayback from "../components/webPlayback";
 
 export default function Root() {
-  const [token, setToken] = useState<string>("");
+  const [token, setToken] = useState("");
 
   useEffect(() => {
-    const localToken = localStorage.getItem("token") || "";
-    setToken(localToken);
+    async function getToken() {
+      const response = await fetch("http://localhost:1314/auth/token", {
+        credentials: "include",
+      });
+      const json = await response.json();
+      console.log(json);
+      setToken(json.token);
+    }
+
+    getToken();
   }, []);
 
   return <>{token === "" ? <Login /> : <WebPlayback token={token} />}</>;
