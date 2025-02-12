d36b63a239d7aa5d7ff0f01a8d30b50b2cbeb18f
biggestUaena24 <a2782541671@gmail.com>
2025-02-12 01:49:03 -0500

    new landing page

--- src/page/loginPage.tsx
+++ src/page/loginPage.tsx
@@ -2,20 +2,11 @@ import LoginButton from "../components/loginButton";
 
 export default function LoginPage() {
   return (
-    <div>
-      <nav>
-        <nav className="bg-black-background border-gray-200 dark:bg-gray-900">
-          <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
-              Guess The Song!
-            </span>
-            <LoginButton />
-          </div>
-        </nav>
-      </nav>
-      <div>
-        There should be some contents displayed here to show what this website
-      </div>
+    <div className="min-h-screen bg-gradient-to-b from-black-background to-purple-900 flex flex-col items-center justify-center">
+      <h1 className="text-6xl font-semibold text-white mb-16">
+        Guess The Song!
+      </h1>
+      <LoginButton />
     </div>
   );
 }
