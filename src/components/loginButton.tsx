d39057e0b3d58aecb2cd15c110c57cf4ef3137c0
biggestUaena24 <a2782541671@gmail.com>
2025-02-16 16:08:02 -0500

    adding more style

--- src/components/loginButton.tsx
+++ src/components/loginButton.tsx
@@ -1,6 +1,6 @@
 export default function Login() {
   return (
-    <div className="text-white bg-contrast-green hover:bg-contrast-green-focus focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+    <div className="text-white bg-contrast-green hover:bg-contrast-green-focus focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-2xl text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
       <header className="App-header">
         <a className="btn-spotify" href="https://localhost:1314/auth/login">
           Login with Spotify
--- src/routes/gamepage.tsx
+++ src/routes/gamepage.tsx
@@ -8,15 +8,22 @@ function GamePage() {
   };
 
   return (
-    <>
-      <h1>
+    <div className="min-h-screen bg-gradient-to-b from-black-background to-purple-900 flex flex-col items-center justify-center">
+      <h1 className="text-2xl font-semibold text-white mb-16">
         Welcome to the game page! Choose how many songs that you want to play
         randomly?
       </h1>
-      <button onClick={() => handleClick(20)}>20</button>
-      <button onClick={() => handleClick(30)}>30</button>
-      <button onClick={() => handleClick(50)}>50</button>
-    </>
+      <div className="flex items-center justify-items-stretch rounded-2xl">
+        <button
+          className="text-white bg-contrast-green hover:bg-contrast-green-focus focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-2xl text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+          onClick={() => handleClick(20)}
+        >
+          20
+        </button>
+        <button onClick={() => handleClick(30)}>30</button>
+        <button onClick={() => handleClick(50)}>50</button>
+      </div>
+    </div>
   );
 }
 
